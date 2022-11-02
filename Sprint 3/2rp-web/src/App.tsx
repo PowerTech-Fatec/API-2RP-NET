819cb24283a0d434e0eca1839e5ef6ba8ecd83d1
fredrbo <fred.rbo23@gmail.com>
2022-11-02 19:28:51 -0300

    arquivos tsx

--- Sprint 3/2rp-web/src/App.tsx
+++ Sprint 3/2rp-web/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
-import Login from './pages/login/login'
 import Dashboard from './pages/home/dashboard'
 import Sobreaviso from './pages/home/sobreaviso/sobreaviso'
 import HoraExtra from './pages/home/hora-extra/hora-extra'
 import Gestor from './pages/home/gestor/aprovar-hora-extra/gestor'
 import GestorSobreaviso from './pages/home/gestor/aprovar-sobreaviso/gestor-sobreaviso'
 import GestorHistorico from './pages/home/gestor/historico/gestor-historico'
+import { Login } from './pages/login/login';
 
 function App() {
   return (
