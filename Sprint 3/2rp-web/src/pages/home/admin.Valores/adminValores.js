1a28afaeb96740649573c2041ccc430bb923d9a9
Carlos Henrique Benício Costa <ique.benicio@hotmail.com>
2022-11-06 17:14:21 -0300

    modified:   Sprint 3/2rp-web/src/components/sidebar.js 	modified:   Sprint 3/2rp-web/src/pages/home/admin.Valores/adminValores.css 	modified:   Sprint 3/2rp-web/src/pages/home/admin.Valores/adminValores.js

--- Sprint 3/2rp-web/src/pages/home/admin.Valores/adminValores.js
+++ Sprint 3/2rp-web/src/pages/home/admin.Valores/adminValores.js
@@ -15,7 +15,7 @@ function adminValores() {
 
         <div>
           <span className="filtroLabel">
-              <label className="mt-4 mb-4 ml-2 filtro font">Usuário</label>
+              <input placeholder="Usuário" className="mt-4 mb-4 ml-2 filtro font"></input>
           </span>
           {/* card info hora extra */}
         </div>
@@ -31,7 +31,7 @@ function adminValores() {
 
         </div>
         <div className="d-flex aling-itens-center">
-          <label className="espaco tela offset-1">1069</label>
+          <label className="espaco tela offset-1">1601</label>
           <label className="espaco tela">10</label>
           <label className="espaco tela">15,10</label>
           <label className="espaco tela">15,10</label>
@@ -40,13 +40,40 @@ function adminValores() {
           <label className="espaco tela">25</label>
         </div>
         <div className="d-flex aling-itens-center">
-          <label className="espaco2 tela offset-1">1069</label>
-          <label className="espaco2 tela"></label>
-          <label className="espaco2 tela"></label>
-          <label className="espaco2 tela"></label>
-          <label className="espaco2 tela"></label>
-          <label className="espaco2 tela"></label>
-          <label className="espaco2 tela"></label>
+          <label className="espaco tela offset-1">1602</label>
+          <label className="espaco tela">7</label>
+          <label className="espaco tela">9,30</label>
+          <label className="espaco tela">7,50</label>
+          <label className="espaco tela">12</label>
+          <label className="espaco tela">15</label>
+          <label className="espaco tela">13</label>
+        </div>
+        <div className="d-flex aling-itens-center">
+          <label className="espaco tela offset-1">1809</label>
+          <label className="espaco tela">15</label>
+          <label className="espaco tela">8,23</label>
+          <label className="espaco tela">4,58</label>
+          <label className="espaco tela">17</label>
+          <label className="espaco tela">26</label>
+          <label className="espaco tela">9</label>
+        </div>
+        <div className="d-flex aling-itens-center">
+          <label className="espaco tela offset-1">3000</label>
+          <label className="espaco tela">36</label>
+          <label className="espaco tela">21,10</label>
+          <label className="espaco tela">2,39</label>
+          <label className="espaco tela">19</label>
+          <label className="espaco tela">18</label>
+          <label className="espaco tela">17</label>
+        </div>
+        <div className="d-flex aling-itens-center">
+          <label className="espaco2 tela offset-1">3001</label>
+          <label className="espaco2 tela">11</label>
+          <label className="espaco2 tela">16,10</label>
+          <label className="espaco2 tela">11,10</label>
+          <label className="espaco2 tela">22</label>
+          <label className="espaco2 tela">27</label>
+          <label className="espaco2 tela">25</label>
         </div>
 
       </div>
